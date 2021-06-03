6b4d0b958bbaaf98d81a16ae4b991facba8e2b23
Guillaume Soisson <guillaume.soisson@epitech.eu>
2021-06-03 15:23:10 +0200

    Fix crash

--- client/src/core.c
+++ client/src/core.c
@@ -47,8 +47,11 @@ int command_handler_send(client_t *client)
     char *command = NULL;
 
     command = get_command();
-    if (command == NULL)
+    if (command == NULL) {
+        send_command(client, "/logout");
+        command_handler(client);
         return (1);
+    }
     send_command(client, command);
     command_handler(client);
     free(command);
@@ -61,6 +64,7 @@ int select_socket(client_t *client, fd_set *set)
 
     select_return = select(FD_SETSIZE, set, NULL, NULL, NULL);
     if (select_return == -1) {
+        printf("yo\n");
         return (1);
     } else if (select_return > 0) {
         for (int i = 0; i < FD_SETSIZE; i++) {
